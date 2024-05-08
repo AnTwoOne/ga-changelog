a42d14d1de0412f98e22fb7e47e5f8d64addb7bd
AnTwoOne <79530233+AnTwoOne@users.noreply.github.com>
2024-05-08 15:37:23 +0000

    rss

--- api/rss.js
+++ api/rss.js
@@ -13,20 +13,36 @@ module.exports = async (req, res) => {
         site_url: 'https://www.getaccept.com/',
         language: 'en',
         pubDate: new Date().toUTCString(),
+        custom_namespaces: {
+            'media': 'http://search.yahoo.com/mrss/'
+        }
     });
 
     response.data.results.forEach(item => {
       const tags = item.values.tags.map(tag => tag.name)
-      console.log(typeof tags, typeof [...tags], typeof [...new Set(tags)], "first tag", tags[0])
-    //   const tagsArr = Array.from(tags)
       feed.item({
         title: item.values.title,
         description: item.values.summary,
-        enclosure: {url:item.values.card_image.url},
-        categories: [...new Set(tags)],
+        enclosure: {url:item.values.card_image.url, type: 'image/webp'},
+        category: `${tags[0]}`,
         url: item.values.link, // assuming 'link' is the URL
         guid: item.id,
         date: item.publishedAt, // assuming 'pubDate' is the publish date
+        custom_elements: [
+            {
+                'media:content': {
+                    _attr: {
+                        url: item.values.card_image.url,
+                        type: item.values.card_image.type,
+                        width: item.values.card_image.width,
+                        height: item.values.card_image.height
+                    }
+                },
+                'media:description': {
+                    _cdata: item.values.card_image.alt
+                }
+            }
+        ]
       });
     });
 
