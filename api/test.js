499a1ab027b2c96600a32a571b9ee806018e27d8
AnTwoOne <79530233+AnTwoOne@users.noreply.github.com>
2024-05-08 14:18:53 +0000

    changed test

--- api/test.js
+++ api/test.js
@@ -1,9 +1,3 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-
-app.get('/test', (req, res) => {
-  res.json({ message: "Hello from Vercel!" });
-});
-
-module.exports = serverless(app);
+module.exports = async (req, res) => {
+    res.status(200).json({ message: "Hello from test.js!" });
+};
