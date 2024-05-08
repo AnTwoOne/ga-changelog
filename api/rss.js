e8795e712648c83f5a443d1d268ef810a8cbdeb7
AnTwoOne <79530233+AnTwoOne@users.noreply.github.com>
2024-05-08 12:44:52 +0000

    try to fix for vercel

--- api/rss.js
+++ api/rss.js
@@ -1,4 +1,12 @@
-const app = require('../lib/rssGenerator');
+const express = require('express');
 const serverless = require('serverless-http');
+const app = express();
+const rssGenerator = require('../lib/rssGenerator'); // Assuming rssGenerator exports the express app.
+
+app.use('/api', rssGenerator);
+
+app.get('/test', (req, res) => {
+    res.send('Serverless function is working!');
+  });
 
 module.exports = serverless(app);
