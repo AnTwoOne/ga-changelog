12b9dbec14d0e81c4675be0e783c3e64ff692587
AnTwoOne <79530233+AnTwoOne@users.noreply.github.com>
2024-05-08 13:06:48 +0000

    restart

---
+++ api/feed.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const axios = require('axios');
+const app = express();
+
+app.get('/feed.json', async (req, res) => {
+  try {
+    const response = await axios.get('https://api.hubapi.com/cms/v3/hubdb/tables/7589438/rows?portalId=541808');
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
+});
+
+module.exports = serverless(app);
