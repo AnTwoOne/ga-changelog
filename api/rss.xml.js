30a80f0d510d026d3a6fce9b4482ef998e9453f7
AnTwoOne <79530233+AnTwoOne@users.noreply.github.com>
2024-05-24 07:56:42 +0000

    .

--- api/rss.xml.js
+++ api/rss.xml.js
@@ -3,50 +3,53 @@ const RSS = require('rss');
 
 module.exports = async (req, res) => {
   // Retrieve the tag from the query parameter, if provided
-  const categoriesFilter = req.query.categories ? encodeURIComponent(req.query.categories) : '';
-  const tagsFilter = req.query.tags ? encodeURIComponent(req.query.tags) : '';
+  const categoriesFilter = req.query.categories ? req.query.categories : '';
+  const tagsFilter = req.query.tags ? req.query.tags : '';
 
-  console.log(tagsFilter, categoriesFilter)
+  console.log(`Categories Filter: ${categoriesFilter}`);
+  console.log(`Tags Filter: ${tagsFilter}`);
 
   // Build the API URL with optional tag filtering
-  const apiUrl = `https://api.hubapi.com/cms/v3/hubdb/tables/18745726/rows?portalId=541808${categoriesFilter ? `&categories__contains=${categoriesFilter}` : ''}${tagsFilter ? `&tags__contains=${tagsFilter}` : ''}`;
+  const apiUrl = `https://api.hubapi.com/cms/v3/hubdb/tables/18745726/rows?portalId=541808${categoriesFilter ? `&categories__contains=${encodeURIComponent(categoriesFilter)}` : ''}${tagsFilter ? `&tags__contains=${encodeURIComponent(tagsFilter)}` : ''}`;
 
-  console.log(apiUrl)
+  console.log(`API URL: ${apiUrl}`);
 
   try {
     const response = await axios.get(apiUrl);
+    console.log('API response:', response.data);
+
     let feed = new RSS({
-        title: `Changelog result ${categoriesFilter ? `: ${req.query.categories}` : ''} ${tagsFilter ? `& : ${req.query.tags}` : ''}`,  // Adjust title to indicate filtered content
-        description: 'GetAccept Changelog',
-        feed_url: `https://ga-changelog.vercel.app/api/rss.xml`,
-        site_url: 'https://www.getaccept.com/',
-        language: 'en',
-        pubDate: new Date().toUTCString(),
-        custom_namespaces: {
-            'content': 'http://purl.org/rss/1.0/modules/content/'
-        }
+      title: `Changelog result${categoriesFilter ? `: ${req.query.categories}` : ''}${tagsFilter ? ` & : ${req.query.tags}` : ''}`,  // Adjust title to indicate filtered content
+      description: 'GetAccept Changelog',
+      feed_url: `https://ga-changelog.vercel.app/api/rss.xml`,
+      site_url: 'https://www.getaccept.com/',
+      language: 'en',
+      pubDate: new Date().toUTCString(),
+      custom_namespaces: {
+        'content': 'http://purl.org/rss/1.0/modules/content/'
+      }
     });
 
     response.data.results.forEach(item => {
-      let media = ""
-      if(item.values.feature_image) {
+      let media = "";
+      if (item.values.feature_image) {
         media = `
           <div class="hs-featured-image-wrapper"> <a href="${item.values.link}" title="" class="hs-featured-image-link"> <img src="${item.values.feature_image.url}" alt="${item.values.feature_image.alt}" class="hs-featured-image" style="width:auto !important; max-width:50%; float:left; margin:0 15px 15px 0;"> </a> </div>
-        `
+        `;
       }
       feed.item({
         title: item.values.title,
         description: item.values.description,
-        categories: item.values.categories.map(categories => categories.name),
+        categories: item.values.categories ? item.values.categories.map(category => category.name) : [],
         url: item.values.link,
         guid: item.values.link,
         date: new Date(item.values.display_date).toUTCString(),
         custom_elements: [
-            {
-                'content:encoded': {
-                    _cdata: `<div style="display: flex; flex-direction: column; gap: 30px;">${media}<p>${item.values.description}</p></div>`
-                }
+          {
+            'content:encoded': {
+              _cdata: `<div style="display: flex; flex-direction: column; gap: 30px;">${media}<p>${item.values.description}</p></div>`
             }
+          }
         ]
       });
     });
@@ -55,6 +58,7 @@ module.exports = async (req, res) => {
     res.send(feed.xml({ indent: true }));
   } catch (error) {
     console.error('Error fetching or generating RSS:', error);
-    res.status(500). send('Failed to generate RSS feed.');
+    res.status(500).send('Failed to generate RSS feed.');
   }
 };
+
